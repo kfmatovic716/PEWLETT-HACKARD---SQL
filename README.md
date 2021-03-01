3a65ba6b5c121178a96b1800fe27a88775515e23
kfmatovic716 <jabuka611@gmail.com>
2021-02-28 17:29:08 -0800

    Update queries.sql and README

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Pewlett_Hackard-SQL
+
+## Backgound
+
+As a new Data Engineer at Pewlett Hackard, the first major task is a research project on employees of the corporation from the 1980s and 1990s. All that remain of the database of employees from that period are six CSV files, which are used in this project.
+
+The tasks in this project involve designing tables to hold data in CSVs, importing CSVs into a SQL database, and answering questions about the data. These activities relates to analytics engineering performing data modeling, data engineering and data analysis. 
+
+### Data Modeling
+
+    > Inspected the 6 CSV files and sketched out an Entity Relationship Diagram of the tables. [see ERD.png (image) or ERD_code.txt (code)]
+
+### Data Engineering
+
+    > Created a table schema for each of the six CSV files
+    > Created tables in correct order to handle foreign keys appropriately
+    > Imported each CSV file into the corresponding SQL table in the same order that the tables were created and accounted for the headers when importing to avoid errors.
+    > Specified data types, primary keys, foreign keys, and other constraints
+    > Saved schema in a file called `schema.sql`, which can be run once to create and populate all base tables in the database
+
+### Data Analysis
+
+    > Wrote queries to select the following information from the database:
+
+        1. Select the employee number, last name, first name, sex, and salary of all employees, ordered by employee number
+
+        2. Select the first name, last name, and hiring date for all employees hired in 1986
+
+        3. Select the department number, department name, employee number, last name, and first name of all managers of each department
+
+        4. Select the employee number, last name, first name, and department of each employee, ordered by employee number
+
+        5. Select first name, last name, birth date, and sex of all employees whose first name is "Hercules" and last name begins with a "B"
+
+        6. Select employee number, last name, first name, and department name of all employees in the Sales department, ordered by employee number
+
+        7. Select employee number, last name, first name, and department name of all employees in the 'Sales' OR the 'Development' department, ordered by employee number
+
+        8. Count the number of employees, grouped by last name
+
+    > Saved the queries in a file called `queries.sql`
--- queries.sql
+++ queries.sql
@@ -1,25 +1,124 @@
--- Select the employee number, last name, first name, sex, and salary of all employees, ordered by employee number
-SELECT 
+-- 1. Select the employee number, last name, first name, sex, and salary of all employees, ordered by employee number
+
+-- Count number of records in employees and salaries table before joining
+-- Each table counts = 300,024 records
+SELECT
+	COUNT(*)
 FROM
-ORDER BY 
+	employees;
+	
+SELECT
+	COUNT(*)
+FROM
+	salaries;
+	
+-- Join tables: employees & salaries (only include columns of interest)
+-- Sorted by employee number
+SELECT 
+	employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.sex,
+	salaries.salary
+FROM 
+	employees
+INNER JOIN salaries ON
+	employees.emp_no = salaries.emp_no
+ORDER BY
+	employees.emp_no ASC;
 
--- Select the first name, last name, and hiring date for all employees hired in 1986 
+-- 2. Select the first name, last name, and hiring date for all employees hired in 1986 
+SELECT 
+	employees.first_name,
+	employees.last_name,
+	employees.hire_date
+FROM
+	employees
+WHERE
+	DATE_PART('year', hire_date) = 1986;
 
+-- 3. Select the department number, department name, employee number, last name, and first name of all managers of each department
+-- Join employees
+SELECT 
+	employees.emp_no,
+	employees.last_name,
+	employees.first_name,
+	dept_manager.dept_no as department_number,
+	departments.dept_name as department_name
+FROM
+	employees
+JOIN dept_manager ON 
+	employees.emp_no = dept_manager.emp_no
+JOIN departments ON
+	dept_manager.dept_no = departments.dept_no;
 
--- Select the department number, department name, employee number, last name, and first name of all managers of each department
+-- 4. Select the employee number, last name, first name, and department of each employee, ordered by employee number
+SELECT 
+	employees.emp_no as employee_number,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name as department_name
+FROM
+	employees
+JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+JOIN departments ON
+	dept_emp.dept_no = departments.dept_no
+ORDER BY
+	employees.emp_no ASC;
 
+-- 5. Select first name, last name, birth date, and sex of all employees whose first name is "Hercules" and last name begins with a "B"
+SELECT 
+	employees.last_name,
+	employees.first_name,
+	employees.sex,
+	employees.birth_data as birth_date
+FROM
+	employees
+WHERE
+	employees.first_name = 'Hercules'
+	AND employees.last_name LIKE 'B%';
 
--- Select the employee number, last name, first name, and department of each employee, ordered by employee number
+-- 6. Select employee number, last name, first name, and department name of all employees in the Sales department, ordered by employee number
+SELECT
+	employees.emp_no as employee_number,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name as department_name
+FROM
+	employees
+JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+JOIN departments ON
+	dept_emp.dept_no = departments.dept_no	
+WHERE
+	departments.dept_name = 'Sales'
+ORDER BY
+	employee_number ASC;
 
+-- 7. Select employee number, last name, first name, and department name of all employees in the 'Sales' OR the 'Development' department, ordered by employee number
+SELECT
+	employees.emp_no as employee_number,
+	employees.last_name,
+	employees.first_name,
+	departments.dept_name as department_name
+FROM
+	employees
+JOIN dept_emp ON
+	employees.emp_no = dept_emp.emp_no
+JOIN departments ON
+	dept_emp.dept_no = departments.dept_no	
+WHERE
+	departments.dept_name = 'Sales'
+	OR departments.dept_name = 'Development'
+ORDER BY
+	employee_number ASC;
 
--- Select first name, last name, birth date, and sex of all employees whose first name is "Hercules" and last name begins with a "B"
-
-
--- Select employee number, last name, first name, and department name of all employees in the Sales department, ordered by employee number
-
-
--- Select employee number, last name, first name, and department name of all employees in the 'Sales' OR the 'Development' department, ordered by employee number
-
-
--- Count the number of employees, grouped by last name
-
+-- 8. Count the number of employees, grouped by last name
+SELECT
+	employees.last_name,
+	COUNT(*) as num_employees
+FROM
+	employees
+GROUP BY 
+	employees.last_name;
